69cc44cca2de33b97dd4906e656770b88f2a3ef1
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2024-03-31 13:30:00 +0530

    A

--- model/includes/header.php
+++ model/includes/header.php
@@ -42,7 +42,7 @@ Don't miss out on the opportunity to give your child the best possible education
 <meta property="og:site_name" content="The Jain International School - KGF">
 <meta property="article:published_time" content="2024-03-16T00:00:00+00:00">
 <meta property="article:modified_time" content="2024-03-16T00:00:00+00:00">
-<meta property="og:image" content="https://thejaininternationalschool.in/images/logo-meta1200X628_1.png">
+<meta property="og:image" content="./images/logo-meta1200X628_1.png">
 <meta property="og:image:width" content="1200">
 <meta property="og:image:height" content="628">
 <meta property="og:image:type" content="image/jpeg">
