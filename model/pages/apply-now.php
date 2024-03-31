a49dfc00f5c2e603ad38232694143c0beea1bb42
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2024-03-31 14:46:10 +0530

    A

--- model/pages/apply-now.php
+++ model/pages/apply-now.php
@@ -22,28 +22,78 @@
 
           <div class="col-lg-12 mt-5 mt-lg-0">
 
-            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-              <div class="row">
-                <div class="col-md-6 form-group">
-                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
-                </div>
-                <div class="col-md-6 form-group mt-3 mt-md-0">
-                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
-                </div>
-              </div>
-              <div class="form-group mt-3">
-                <input type="text" class="form-control" name="subject" id="subject" placeholder="Mobile Number" required>
-              </div>
-              <div class="form-group mt-3">
-                <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
-              </div>
-              <div class="my-3">
-                <div class="loading">Loading</div>
-                <div class="error-message"></div>
-                <div class="sent-message">Your message has been sent. Thank you!</div>
-              </div>
-              <div class="text-center"><button type="submit">Send Message</button></div>
-            </form>
+          <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+    <!-- Basic Information -->
+    <div class="row">
+        <div class="col-md-6 form-group">
+            <input type="text" name="full_name" class="form-control" placeholder="Applicant's Full Name" required>
+        </div>
+        <div class="col-md-6 form-group">
+            <input type="date" name="date_of_birth" class="form-control" placeholder="Date of Birth" required>
+        </div>
+        <div class="col-md-6 form-group">
+            <select name="gender" class="form-select" required>
+                <option value="">Select Gender</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="other">Other</option>
+            </select>
+        </div>
+        <div class="col-md-6 form-group">
+            <input type="text" name="nationality" class="form-control" placeholder="Nationality" required>
+        </div>
+        <div class="col-md-6 form-group">
+            <input type="text" name="address" class="form-control" placeholder="Address" required>
+        </div>
+        <div class="col-md-6 form-group">
+            <input type="tel" name="phone_number" class="form-control" placeholder="Phone Number" required>
+        </div>
+        <div class="col-md-6 form-group">
+            <input type="email" name="email" class="form-control" placeholder="Email Address" required>
+        </div>
+        <div class="col-md-6 form-group">
+            <input type="text" name="parent_name" class="form-control" placeholder="Parent/Guardian Name" required>
+        </div>
+        <div class="col-md-6 form-group">
+            <input type="tel" name="parent_phone_number" class="form-control" placeholder="Parent/Guardian Phone Number" required>
+        </div>
+    </div>
+    <!-- Previous School Information -->
+    <div class="row">
+        <!-- Fields for previous school information -->
+    </div>
+    <!-- Program and Grade Applying For -->
+    <div class="row">
+        <!-- Fields for program and grade applying for -->
+    </div>
+    <!-- Documents Upload -->
+    <div class="row">
+        <!-- Fields for document uploads -->
+    </div>
+    <!-- Additional Information -->
+    <div class="row">
+        <!-- Fields for additional information -->
+    </div>
+    <!-- Parent/Guardian Information -->
+    <div class="row">
+        <!-- Fields for parent/guardian information -->
+    </div>
+    <!-- Application Fee (if applicable) -->
+    <div class="row">
+        <!-- Fields for application fee (if applicable) -->
+    </div>
+    <!-- Agreements and Declarations -->
+    <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="" id="consent_checkbox" required>
+        <label class="form-check-label" for="consent_checkbox">
+            I consent to the data processing and privacy policy.
+        </label>
+    </div>
+    <!-- Submission Confirmation -->
+    <div class="text-center">
+        <button type="submit" class="btn btn-primary">Submit Application</button>
+    </div>
+</form>
 
           </div>
 
