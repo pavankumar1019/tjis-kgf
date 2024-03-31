39872218e7b2d0970e77af021538be547f63c56e
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2024-03-31 19:15:31 +0530

    A

--- api/main.php
+++ api/main.php
@@ -80,8 +80,7 @@ if ($method == 'POST') {
   	<meta name="viewport" content="width=device-width, initial-scale=1.0;">
  	<meta name="format-detection" content="telephone=no"/>
 
-	<!-- Responsive Mobile-First Email Template by Konstantin Savchenko, 2015.
-	https://github.com/konsav/email-templates/  -->
+
 
 	<style>
 /* Reset styles */ 
@@ -225,7 +224,7 @@ a, a:hover {
 				<table border="0" cellpadding="0" cellspacing="0" align="center" style="max-width: 240px; min-width: 120px; border-collapse: collapse; border-spacing: 0; padding: 0;"><tr><td align="center" valign="middle" style="padding: 12px 24px; margin: 0; text-decoration: underline; border-collapse: collapse; border-spacing: 0; border-radius: 4px; -webkit-border-radius: 4px; -moz-border-radius: 4px; -khtml-border-radius: 4px;"
 					bgcolor="#E9703E"><a target="_blank" style="text-decoration: underline;
 					color: #FFFFFF; font-family: sans-serif; font-size: 17px; font-weight: 400; line-height: 120%;"
-					href="https://github.com/konsav/email-templates/">
+					href="tel:+919845498660">
 						Enquiry Now
 					</a>
 			</td></tr></table></a>
