9f4d59233f5ba6fce6645e8d8a0c80866b99c0c4
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2024-03-31 14:36:28 +0530

    A

---
+++ model/pages/apply-now.php
@@ -0,0 +1,102 @@
+<!-- apply-now -->
+
+<main id="main">
+
+<!-- ======= About Section ======= -->
+<section id="about" class="about">
+  <div class="container" data-aos="fade-up">
+
+    <div class="section-title">
+      <h2>About</h2>
+      <p>About The Jain International School
+        "The Legacy"</p>
+    </div>
+
+    <div class="row">
+      <div class="col-lg-4">
+        <img src="https://tjiskgf.com/wp-content/uploads/2021/08/003.jpg" class="img-fluid" alt="">
+      </div>
+      <div class="col-lg-8 pt-4 pt-lg-0 content">
+        <h3>The Jain International School</h3>
+   
+      
+        <p>
+          Founded by JGI, The Jain International School is one of the Best Schools in K.G.F aims for a holistic approach to educate and guide students with Karnataka State Board curriculum and that ensures comprehensive learning experience. Our school is a co-educational, day-boarding school. We set the highest standards for academic achievement, intellectual growth, ethical awareness and behavior, and sportsmanship for our students.
+
+        </p>
+      </div>
+    </div>
+
+  </div>
+</section><!-- End About Section -->
+
+
+
+
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Contact</h2>
+        </div>
+
+        <div class="row mt-1">
+
+          <div class="col-lg-4">
+            <div class="info">
+              <div class="address">
+                <i class="bi bi-geo-alt"></i>
+                <h4>Location:</h4>
+                <p>Doddurkarpanahalli Plantation, Krishnapuram Road, Kolar Gold Fields, Doddurkarpanahalli Plantation OG, Karnataka 563121</p>
+              </div>
+
+              <div class="email">
+                <i class="bi bi-envelope"></i>
+                <h4>Email:</h4>
+                <p>thejaininternationalschoolkgf@gmail.com</p>
+              </div>
+
+              <div class="phone">
+                <i class="bi bi-phone"></i>
+                <h4>Call:</h4>
+                <p>+919845498660</p>
+              </div>
+
+            </div>
+
+          </div>
+
+          <div class="col-lg-8 mt-5 mt-lg-0">
+
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+              <div class="row">
+                <div class="col-md-6 form-group">
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+                </div>
+                <div class="col-md-6 form-group mt-3 mt-md-0">
+                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+                </div>
+              </div>
+              <div class="form-group mt-3">
+                <input type="text" class="form-control" name="subject" id="subject" placeholder="Mobile Number" required>
+              </div>
+              <div class="form-group mt-3">
+                <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+              </div>
+              <div class="my-3">
+                <div class="loading">Loading</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Your message has been sent. Thank you!</div>
+              </div>
+              <div class="text-center"><button type="submit">Send Message</button></div>
+            </form>
+
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+</main><!-- End #main -->
