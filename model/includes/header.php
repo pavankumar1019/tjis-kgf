9948f65ced81e7da228e4118f9a2df020ab5dcb5
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2024-03-31 13:08:53 +0530

    A

--- model/includes/header.php
+++ model/includes/header.php
@@ -38,8 +38,8 @@ $URL = array_values(array_filter($URL));
 
 
   <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="./images/JGI_logo___Low.png" rel="icon">
+  <link href="./images/JGI_logo___Low.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
