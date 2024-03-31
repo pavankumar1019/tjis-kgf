81f882e7706d80bda65efdfecad93694cfd4671a
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2024-03-31 17:17:34 +0530

    A

--- config/database.php
+++ config/database.php
@@ -1,16 +1,16 @@
 <?php
-// $servername = "localhost";
-// $username = "u769900516_pavan";
-// $password = "Kumar@5639";
-// $dbname = "u769900516_sciento";
+$servername = "localhost";
+$username = "u736026977_tjis";
+$password = "Tjiskgf@5639";
+$dbname = "u736026977_tjis";
 
-// // Create connection
-// $conn = new mysqli($servername, $username, $password, $dbname);
-// // Check connection
-// if ($conn->connect_error) {
-//   die("Connection failed: " . $conn->connect_error);
-// }
-// $connect = new PDO("mysql:host=localhost; dbname=u769900516_sciento", "u769900516_pavan", "Kumar@5639");
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$connect = new PDO("mysql:host=localhost; dbname=u769900516_sciento", "u769900516_pavan", "Kumar@5639");
 
 
 // https://auth-db1112.hstgr.io/
