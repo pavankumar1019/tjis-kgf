7ba5829a9312eb1c1755ef81b72744efe4799f62
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2024-03-31 17:48:37 +0530

    A

--- api/main.php
+++ api/main.php
@@ -25,19 +25,29 @@ if ($method == 'POST') {
 
   if ($_POST['type'] == "insert-course") {
     // Sanitize and escape user inputs
-    $categories_id = mysqli_real_escape_string($conn, $_POST['categories_id']);
-    $extended_title = mysqli_real_escape_string($conn, $_POST['extended_title']);
-    $overview = mysqli_real_escape_string($conn, $_POST['overview']);
-    $description = mysqli_real_escape_string($conn, $_POST['description']);
+    $full_name = mysqli_real_escape_string($conn, $_POST['full_name']);
+    $dob = mysqli_real_escape_string($conn, $_POST['dob']);
+    $gender = mysqli_real_escape_string($conn, $_POST['gender']);
+    $nationality = mysqli_real_escape_string($conn, $_POST['nationality']);
+    $address = mysqli_real_escape_string($conn, $_POST['address']);
+    $contact = mysqli_real_escape_string($conn, $_POST['contact']);
+    $alt_contact = mysqli_real_escape_string($conn, $_POST['alt_contact']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $p_g_name = mysqli_real_escape_string($conn, $_POST['p_g_name']);
+    $p_g_contact = mysqli_real_escape_string($conn, $_POST['p_g_contact']);
+    $pre_sch_name = mysqli_real_escape_string($conn, $_POST['pre_sch_name']);
+    $pre_sch_address = mysqli_real_escape_string($conn, $_POST['pre_sch_address']);
+    $class_last_attended = mysqli_real_escape_string($conn, $_POST['class_last_attended']);
+    $grade_for_apply = mysqli_real_escape_string($conn, $_POST['grade_for_apply']);
+
 
     // Check if files were uploaded
     if (isset($_FILES["image"]["tmp_name"])) {
         // Read and encode uploaded image and certificate files
-        $file = 'data:image/png;base64,' . base64_encode(file_get_contents($_FILES["image"]["tmp_name"]));
+        $photo = 'data:image/png;base64,' . base64_encode(file_get_contents($_FILES["image"]["tmp_name"]));
         // Create the SQL query to insert the course data
-        $sql = "INSERT INTO newapplications (category_id, title, short_description, long_description, image_1, image_2, image_3, og_image) 
-                VALUES ('$categories_id', '$extended_title', '$overview', '$description',  '$certificate', '$file', '$badge_icon', ' $og')";
-
+        $sql = "INSERT INTO newapplications (full_name, dob, gender, nationality, address, contact, alt_contact, email, p_g_name, p_g_contact, pre_sch_name, pre_sch_address, class_last_attended, grade_for_apply, photo) 
+        VALUES ('$fullname', '$dob', '$gender', '$nationality', '$address', '$contact', '$alt_contact', '$email', '$p_g_name', '$p_g_contact', '$pre_sch_name', '$pre_sch_address', '$class_last_attended', '$grade_for_apply', '$photo')";
         if ($conn->query($sql) === TRUE) {
           $data[] = array("statusCode" => 200, "message" => "Application Submited");
         } else {
