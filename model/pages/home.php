cbc70152ffb8d27580803e3887837a0f8a9ddf2d
tjispavan <166196706+tjispavan@users.noreply.github.com>
2024-05-11 14:40:48 +0530

    A

--- model/pages/home.php
+++ model/pages/home.php
@@ -5,7 +5,7 @@
       <img src="./images/JGI_logo___Low.png" width="100" alt=""> <br> <br>
       <h1>The Jain International School</h1>
       <p>The roots of education are bitter, but the fruit is sweet.</p> <br>
-      <h1>Affiliated to CBSE</h1>
+      <h3>Affiliated to CBSE</h1>
       <p>Affiliation no. 831382</p> <br>
       <button style="background: #0563bb;
       border: 0;
